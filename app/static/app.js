9d5a123713c2b300ffe07969dc36c0762443b6d6
sirCrusader <sercrusader@gmail.com>
2017-07-23 16:04:27 +0300

    Error handling.

--- app/static/app.js
+++ app/static/app.js
@@ -237,14 +237,20 @@ var IssueList = function (_React$Component3) {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(newIssue)
             }).then(function (response) {
-                return response.json();
-            }).then(function (updatedIssue) {
-                updatedIssue.created = new Date(updatedIssue.created);
-                if (updatedIssue.completionDate) {
-                    updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+                if (response.ok) {
+                    response.json().then(function (updatedIssue) {
+                        updatedIssue.created = new Date(updatedIssue.created);
+                        if (updatedIssue.completionDate) {
+                            updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+                        }
+                        var newIssues = _this5.state.issues.concat(updatedIssue);
+                        _this5.setState({ issues: newIssues });
+                    });
+                } else {
+                    response.json().then(function (error) {
+                        console.log("Failed to add issue: " + error.message);
+                    });
                 }
-                var newIssues = _this5.state.issues.concat(updatedIssue);
-                _this5.setState({ issues: newIssues });
             }).catch(function (err) {
                 console.log("Error in sending data to server: " + err.message);
             });
